c231a932ad818dd56f57a999821a5319824ac63b
PankajSre <pankaj.saini85@gmail.com>
2021-10-25 22:14:17 +0530

    Fifth Commit

--- Employee.java
+++ Employee.java
@@ -2,7 +2,5 @@ public class Employee {
    public static void main(String[] args){
       System.out.println("Hello from Employee Class");
 	  System.out.println("This is second call from Employee");
-	  System.out.println("This will execute in Jenkins");
-	  System.out.println("jenkins Automated build cycle");
    }
 }
